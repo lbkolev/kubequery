e617d9ef72ee0ca047762b8677efc9f1f7dcb0df
lbkolev <lachezarkolevgg@gmail.com>
2023-06-25 00:11:04 +0300

    rm commented-out test + lints

--- tests/unit/kubernetes/test_workload.py
+++ tests/unit/kubernetes/test_workload.py
@@ -1,34 +1,27 @@
-#
-# def test_get_attr():
-#    content = yaml.load(open("tests/assets/core-pod.yaml"), Loader=yaml.FullLoader)
-#    V1PodSpec(content["spec"]).containers
-#
-#    deployment = V1Pod(spec=V1PodSpec(content["spec"]), metadata=content["metadata"])
-#
-#    label = rgetattr(deployment, "metadata.labels.app")
-#    assert label == "nginx"
-#
-#
-import pytest
-
 from pprint import pprint
 
-from kubequery.model import Model
+import pytest
+
 from kubequery.client import Client
 from kubequery.kubernetes.resources.workload import Workload
 
 
-class TestWorkload():
-
+class TestWorkload:
     @staticmethod
-    @pytest.mark.parametrize("namespace, attributes, filters, expected", [
-        ("default", ["spec.dns_policy"], {"spec.service_account": "default", "metadata.name": "web-0"}, {"spec.dns_policy": ["ClusterFirst"]}),
-        ("rand", ["metadata.name", "metadata.labels"], {}, {}),
-    ])
+    @pytest.mark.parametrize(
+        "namespace, attributes, filters, expected",
+        [
+            (
+                "default",
+                ["spec.dns_policy"],
+                {"spec.service_account": "default", "metadata.name": "web-0"},
+                {"spec.dns_policy": ["ClusterFirst"]},
+            ),
+            ("rand", ["metadata.name", "metadata.labels"], {}, {}),
+        ],
+    )
     def test_select_pod(namespace, attributes, filters, expected):
-        client = Client(
-            contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters
-        )
+        client = Client(contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters)
         wl = Workload(client)
         resp = wl.select("V1Pod")
 
@@ -37,31 +30,42 @@ class TestWorkload():
         assert resp[0].values == expected
 
     @staticmethod
-    @pytest.mark.parametrize("namespace, attributes, filters, expected", [
-        ("default", ["spec.replicas", "spec.progress_deadline_seconds"], {"metadata.name": "nginx-app"}, {"spec.replicas": [3], "spec.progress_deadline_seconds": [600]}),
-        ("rand", ["metadata.name", "metadata.labels"], {"spec.replicas": 69}, {}),
-    ])
+    @pytest.mark.parametrize(
+        "namespace, attributes, filters, expected",
+        [
+            (
+                "default",
+                ["spec.replicas", "spec.progress_deadline_seconds"],
+                {"metadata.name": "nginx-app"},
+                {"spec.replicas": [3], "spec.progress_deadline_seconds": [600]},
+            ),
+            ("rand", ["metadata.name", "metadata.labels"], {"spec.replicas": 69}, {}),
+        ],
+    )
     def test_select_deployment(namespace, attributes, filters, expected):
-        client = Client(
-            contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters
-        )
+        client = Client(contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters)
         wl = Workload(client)
         resp = wl.select("deployment")
-        
+
         pprint(resp[0].values)
         assert len(resp) == 1
         assert resp[0].values == expected
 
-
     @staticmethod
-    @pytest.mark.parametrize("namespace, attributes, filters, expected", [
-        ("default", ["spec.replicas", "spec.template.spec.scheduler_name"], {"metadata.name": "frontend"}, {"spec.replicas": [3], "spec.template.spec.scheduler_name": ["default-scheduler"]}),
-        ("rand", ["metadata.name", "metadata.labels"], {"spec.replicas": 69}, {}),
-    ])
+    @pytest.mark.parametrize(
+        "namespace, attributes, filters, expected",
+        [
+            (
+                "default",
+                ["spec.replicas", "spec.template.spec.scheduler_name"],
+                {"metadata.name": "frontend"},
+                {"spec.replicas": [3], "spec.template.spec.scheduler_name": ["default-scheduler"]},
+            ),
+            ("rand", ["metadata.name", "metadata.labels"], {"spec.replicas": 69}, {}),
+        ],
+    )
     def test_select_replicaset(namespace, attributes, filters, expected):
-        client = Client(
-            contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters
-        )
+        client = Client(contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters)
         wl = Workload(client)
         resp = wl.select("replicasets")
 
@@ -70,14 +74,15 @@ class TestWorkload():
         assert resp[0].values == expected
 
     @staticmethod
-    @pytest.mark.parametrize("namespace, attributes, filters, expected", [
-        ("default", ["spec.update_strategy.type"], {}, {"spec.update_strategy.type": ["RollingUpdate"]}),
-        ("rand", ["metadata.name", "metadata.labels"], {"spec.replicas": 69}, {}),
-    ])
+    @pytest.mark.parametrize(
+        "namespace, attributes, filters, expected",
+        [
+            ("default", ["spec.update_strategy.type"], {}, {"spec.update_strategy.type": ["RollingUpdate"]}),
+            ("rand", ["metadata.name", "metadata.labels"], {"spec.replicas": 69}, {}),
+        ],
+    )
     def test_select_statefulset(namespace, attributes, filters, expected):
-        client = Client(
-            contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters
-        )
+        client = Client(contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters)
         wl = Workload(client)
         resp = wl.select("statefulset")
 
@@ -86,14 +91,20 @@ class TestWorkload():
         assert resp[0].values == expected
 
     @staticmethod
-    @pytest.mark.parametrize("namespace, attributes, filters, expected", [
-        ("kube-system", ["spec.template.metadata.creation_timestamp"], {"metadata.name": "kube-proxy"}, {"spec.template.metadata.creation_timestamp": [None]}),
-        ("rand", ["metadata.name", "metadata.labels"], {}, {}),
-    ])
+    @pytest.mark.parametrize(
+        "namespace, attributes, filters, expected",
+        [
+            (
+                "kube-system",
+                ["spec.template.metadata.creation_timestamp"],
+                {"metadata.name": "kube-proxy"},
+                {"spec.template.metadata.creation_timestamp": [None]},
+            ),
+            ("rand", ["metadata.name", "metadata.labels"], {}, {}),
+        ],
+    )
     def test_select_daemonset(namespace, attributes, filters, expected):
-        client = Client(
-            contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters
-        )
+        client = Client(contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters)
         wl = Workload(client)
         resp = wl.select("daemonset")
 
@@ -102,14 +113,20 @@ class TestWorkload():
         assert resp[0].values == expected
 
     @staticmethod
-    @pytest.mark.parametrize("namespace, attributes, filters, expected", [
-        ("default", ["spec.completions", "spec.parallelism", "spec.completion_mode"], {"metadata.name": "pi"}, {"spec.completions": [1], "spec.parallelism": [1], "spec.completion_mode": ["NonIndexed"]}),
-        ("rand", ["metadata.name", "metadata.labels"], {}, {}),
-    ])
+    @pytest.mark.parametrize(
+        "namespace, attributes, filters, expected",
+        [
+            (
+                "default",
+                ["spec.completions", "spec.parallelism", "spec.completion_mode"],
+                {"metadata.name": "pi"},
+                {"spec.completions": [1], "spec.parallelism": [1], "spec.completion_mode": ["NonIndexed"]},
+            ),
+            ("rand", ["metadata.name", "metadata.labels"], {}, {}),
+        ],
+    )
     def test_select_job(namespace, attributes, filters, expected):
-        client = Client(
-            contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters
-        )
+        client = Client(contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters)
         wl = Workload(client)
         resp = wl.select("jobs")
 
@@ -118,14 +135,22 @@ class TestWorkload():
         assert resp[0].values == expected
 
     @staticmethod
-    @pytest.mark.parametrize("namespace, attributes, filters, expected", [
-        ("default", ["spec.schedule", ], {"metadata.name": "hello"}, {"spec.schedule": ["* * * * *"]}),
-        ("rand", ["metadata.name", "metadata.labels"], {}, {}),
-    ])
+    @pytest.mark.parametrize(
+        "namespace, attributes, filters, expected",
+        [
+            (
+                "default",
+                [
+                    "spec.schedule",
+                ],
+                {"metadata.name": "hello"},
+                {"spec.schedule": ["* * * * *"]},
+            ),
+            ("rand", ["metadata.name", "metadata.labels"], {}, {}),
+        ],
+    )
     def test_select_cronjob(namespace, attributes, filters, expected):
-        client = Client(
-            contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters
-        )
+        client = Client(contexts=["minikube"], namespace=namespace, attributes=attributes, filters=filters)
         wl = Workload(client)
         resp = wl.select("cronjob")
 
